aacce0a61b1a9bfcb49dfa0d3b6d83fda843dadd
Michal Rogowski <xcanselx@gmail.com>
2026-01-22 15:36:37 +0100

    dodnie plikow na bancha

---
+++ build/googletest-master/googletest-1.17.0/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Programowanie/NetSim/googletest-master/googletest-1.17.0/googlemock
+# Build directory: C:/Programowanie/NetSim/build/googletest-master/googletest-1.17.0/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ build/tests/unit_tests[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "C:/Programowanie/NetSim/build/tests/unit_tests[1]_tests.cmake")
+  include("C:/Programowanie/NetSim/build/tests/unit_tests[1]_tests.cmake")
+else()
+  add_test(unit_tests_NOT_BUILT unit_tests_NOT_BUILT)
+endif()
