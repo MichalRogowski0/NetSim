7624c16371feb47f3d193021b37bf7218011feb4
Michal Rogowski <xcanselx@gmail.com>
2026-01-23 23:15:47 +0100

    cleanup

--- build/googletest-master/googletest-1.17.0/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Programowanie/NetSim/googletest-master/googletest-1.17.0
-# Build directory: C:/Programowanie/NetSim/build/googletest-master/googletest-1.17.0
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/googletest-master/googletest-1.17.0/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Programowanie/NetSim/googletest-master/googletest-1.17.0/googletest
-# Build directory: C:/Programowanie/NetSim/build/googletest-master/googletest-1.17.0/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
