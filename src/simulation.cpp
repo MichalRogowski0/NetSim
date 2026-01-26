403a697bfe0f63cc4e5147e7263bb5f2bac3d530
Kamila Rebilas <kamila.rebilas@gmail.com>
2026-01-26 16:05:55 +0100

    simulation implementation

---
+++ src/simulation.cpp
@@ -0,0 +1,17 @@
+#include "simulation.hpp"
+#include <stdexcept>
+
+void simulate(Factory& f, TimeOffset d, std::function<void(Factory&, Time)>& rf) {
+    if (!f.is_consistent()) {
+        throw std::logic_error("Sieć fabryki jest niespójna")
+    }
+
+    for (Time t = 1; t <= d; ++t) {
+
+        f.do_deliveries(t);
+        f.do_package_passing();
+        f.do_work(t);
+
+        rf(f, t);
+    }
+}
