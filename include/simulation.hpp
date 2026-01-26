a9e33125d7abe32d91f3f008ff1b0ffb2d221d91
Kamila Rebilas <kamila.rebilas@gmail.com>
2026-01-26 15:38:28 +0100

    merge with main, start simulation coding

---
+++ include/simulation.hpp
@@ -0,0 +1,9 @@
+#ifndef SIMULATION_HPP
+#define SIMULATION_HPP
+
+#include "types.hpp"
+#include "factory.hpp"
+
+void simulate(Factory& f, TimeOffset d, std::function<void(Factory&, Time)>& rf);
+
+#endif //SIMULATION_HPP
