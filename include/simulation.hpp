4cbf73c5d7259a63fa20bcb54744f3085304eae2
Kamila Rebilas <kamila.rebilas@gmail.com>
2026-01-26 16:16:16 +0100

    simulation.hpp done

--- include/simulation.hpp
+++ include/simulation.hpp
@@ -3,7 +3,27 @@
 
 #include "types.hpp"
 #include "factory.hpp"
+#include <set>
+#include <functional>
 
 void simulate(Factory& f, TimeOffset d, std::function<void(Factory&, Time)> rf);
+
+class SpecificTurnsReportNotifier {
+public:
+    explicit SpecificTurnsReportNotifier(std::set<Time> turns);
+    bool should_generate_report(Time t) const;
+
+private:
+    std::set<Time> turns_;
+};
+
+class IntervalReportNotifier {
+public:
+    explicit IntervalReportNotifier(TimeOffset interval);
+    bool should_generate_report(Time t) const;
+
+private:
+    TimeOffset interval_;
+};
 
 #endif //SIMULATION_HPP
