e158eec9f7860783293a4726856a51dd16fbb1b8
kamilarebilas <kamila.rebilas@gmail.com>
2026-01-28 13:11:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
 #include "gtest/gtest.h"
+#include "package.hpp"
+#include "storage_types.hpp"
+#include "types.hpp"
+#include "factory.hpp"
+#include "nodes.hpp"
+#include "helpers.hpp"
+
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
